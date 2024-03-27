559633dac93f9649a8d8845fe0f85356ec6643cd
kouwtkz <kouwtkz@cottonwind.com>
2024-03-27 17:46:03 +0900

    スマホのスクロール周りの調整

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,11 +1,12 @@
 import { Outlet, ScrollRestoration, useLocation } from "react-router-dom";
 import { Header } from "../components/layout/Header";
 import { Footer } from "../components/layout/Footer";
-import { ReactNode, useRef } from "react";
+import { ReactNode, useLayoutEffect, useRef } from "react";
 import { StateSet } from "../state/StateSet";
 import { SetTitleStr } from "../data/functions/SetMeta";
 import { useCharaState } from "../state/CharaState";
 import { serverSite as site } from "../data/server/site";
+import { isMobile } from "react-device-detect";
 
 function SetTitle() {
   const { pathname, search } = useLocation();
@@ -39,6 +40,11 @@ export function Base({ children }: { children?: ReactNode }) {
 }
 
 export default function Root() {
+  useLayoutEffect(() => {
+    if (isMobile) {
+      document.body.classList.add("mobile");
+    }
+  });
   return (
     <>
       <ScrollRestoration />
