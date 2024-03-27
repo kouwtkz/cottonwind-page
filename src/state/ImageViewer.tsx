08b00872fa4c54e1081b5f4bcf8828fe83d4c65f
kouwtkz <kouwtkz@cottonwind.com>
2024-03-27 12:20:02 +0900

    ギャラリーのビューアを最適化した

--- src/state/ImageViewer.tsx
+++ src/state/ImageViewer.tsx
@@ -1,4 +1,5 @@
 import {
+  ReactNode,
   useCallback,
   useEffect,
   useLayoutEffect,
@@ -29,6 +30,7 @@ import { RiBook2Fill, RiFilePdf2Fill, RiFullscreenFill } from "react-icons/ri";
 import { useCharaState } from "../state/CharaState";
 import { useImageState } from "./ImageState";
 import { useDataState } from "./StateSet";
+import { useGalleryObject } from "../routes/GalleryPage";
 
 const body = typeof window === "object" ? document?.body : null;
 const bodyLock = (m: boolean) => {
@@ -41,21 +43,15 @@ const bodyLock = (m: boolean) => {
 
 type ImageViewerType = {
   imageSrc: string;
-  albumName: string;
-  albumImages: string[];
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
   editMode: boolean;
   setEditMode: (editMode: boolean) => void;
-  setImageName: (src: string, album?: string | null) => void;
-  setAlbumName: (name: string) => void;
-  setAlbumImages: (list: string[]) => void;
+  setImageName: (src: string) => void;
 };
 export const useImageViewer = create<ImageViewerType>((set) => ({
   imageSrc: "",
-  albumName: "",
-  albumImages: [],
   isOpen: false,
   onOpen: () => {
     set(() => ({ isOpen: true }));
@@ -69,21 +65,13 @@ export const useImageViewer = create<ImageViewerType>((set) => ({
   setEditMode(editMode) {
     set(() => ({ editMode }));
   },
-  setImageName(src, album) {
-    const option: { imageSrc?: string; albumName?: string } = { imageSrc: src };
-    if (typeof album !== "undefined") option.albumName = album || "";
+  setImageName(src) {
     set(() => ({
-      ...option,
+      imageSrc: src,
       albumImages: [],
       isOpen: true,
     }));
     bodyLock(true);
-  },
-  setAlbumName(name) {
-    set(() => ({ albumName: name }));
-  },
-  setAlbumImages(list) {
-    set(() => ({ albumImages: list }));
   },
 }));
 
@@ -91,21 +79,16 @@ export default function ImageViewer() {
   const { imageItemList } = useImageState();
   const { charaList } = useCharaState();
   const nav = useNavigate();
-  const {
-    isOpen,
-    onClose,
-    imageSrc,
-    albumName,
-    setImageName,
-    editMode,
-    setEditMode,
-    albumImages,
-  } = useImageViewer();
+  const { isOpen, onClose, imageSrc, setImageName, editMode, setEditMode } =
+    useImageViewer();
   const { pathname, search: searchStr } = useLocation();
+  const { items } = useGalleryObject();
   const search = new URLSearchParams(searchStr);
   const query = Object.fromEntries(search);
   const imageParam = query.image;
   const albumParam = query.album;
+  const groupParam = query.group ?? albumParam;
+  const galleryItem = items?.find((item) => item.name === groupParam);
   const modeParam = query.mode;
   const isProd = import.meta.env.PROD;
   const isDev = import.meta.env.DEV;
@@ -124,18 +107,13 @@ export default function ImageViewer() {
     }, 10);
   }, [query, nav]);
 
-  const updateFlag = useRef(false);
   useLayoutEffect(() => {
     if (!imageParam) {
       if (isOpen) onClose();
-    } else if (
-      imageParam !== imageSrc ||
-      (albumParam && albumParam !== albumName)
-    ) {
-      setImageName(imageParam, albumParam);
-      updateFlag.current = true;
+    } else {
+      setImageName(imageParam);
     }
-  });
+  }, [imageParam, isOpen]);
   useLayoutEffect(() => {
     switch (modeParam) {
       case "edit":
@@ -163,26 +141,22 @@ export default function ImageViewer() {
       : null;
   }, [imageItemList, albumParam, imageParam, imageSrc]);
 
-  const albumImageItems = useMemo(
-    () =>
-      albumImages
-        .map(
-          (imageURL) =>
-            imageItemList[
-              imageItemList.findIndex(({ URL }) => URL === imageURL)
-            ]
-        )
-        .filter((v) => v),
-    [albumImages, imageItemList]
+  const groupImageList = useMemo(
+    () => galleryItem?.list ?? [],
+    [galleryItem?.list]
   );
-  const imageIndex = albumImageItems.findIndex(({ URL }) => image?.URL === URL);
+
+  const imageIndex = useMemo(
+    () => groupImageList.findIndex(({ URL }) => image?.URL === URL),
+    [image?.URL, groupImageList]
+  );
 
   const beforeAfterImage = useMemo(
     () => ({
-      before: albumImageItems[imageIndex - 1],
-      after: albumImageItems[imageIndex + 1],
+      before: groupImageList[imageIndex - 1],
+      after: groupImageList[imageIndex + 1],
     }),
-    [albumImageItems, imageIndex]
+    [groupImageList, imageIndex]
   );
 
   const titleEqFilename = useMemo(
@@ -195,212 +169,221 @@ export default function ImageViewer() {
     [image]
   );
 
-  const InfoCmp = useMemo(() => {
-    if ("pic" in query || !image?.album?.visible?.info) return <></>;
-    return (
+  const InfoCmp = useCallback(
+    ({ children }: { children?: ReactNode }) => (
       <div className="infoArea">
-        {isComplete ? (
+        {!("pic" in query) && image?.album?.visible?.info ? (
           <>
-            {editMode ? null : (
-              <div className="info window">
-                {image.album.visible.title &&
-                (image.album.visible.filename || !titleEqFilename) ? (
-                  <h2 className="title">{image.name}</h2>
-                ) : (
-                  <div className="title" />
-                )}
-                <div className="description">
-                  <MultiParser>{image.description}</MultiParser>
-                </div>
-                <div className="tagList">
-                  {charaList
-                    .filter((chara) =>
-                      image.tags?.some((tag) => chara.id === tag)
-                    )
-                    .map((chara, i) => {
-                      return (
-                        <Link
-                          to={"/character/" + chara.id}
-                          onClick={() => {
-                            onClose();
-                            return true;
-                          }}
-                          className="character"
-                          key={i}
-                        >
-                          {chara?.media?.icon ? (
-                            <ImageMee
-                              imageItem={chara.media.icon}
-                              mode="icon"
-                              width={40}
-                              height={40}
-                              className="charaIcon"
-                            />
-                          ) : (
-                            <></>
-                          )}
-                          <span className="align-middle">{chara.name}</span>
-                        </Link>
-                      );
-                    })}
-                  {image.tags
-                    ?.filter((tag) =>
-                      tagsOptions.some(({ value }) => value === tag)
-                    )
-                    .map((tag, i) => {
-                      const item = tagsOptions.find(
-                        ({ value }) => value === tag
-                      );
-                      if (!item) return item;
-                      return (
-                        <Link
-                          to={MakeRelativeURL({
-                            ...(pathname.startsWith("/gallery")
-                              ? {}
-                              : { pathname: "/gallery" }),
-                            query: item.query || { tag: item.value },
-                          })}
-                          className="other"
-                          preventScrollReset={false}
-                          key={i}
-                        >
-                          <span>{item.label}</span>
-                        </Link>
-                      );
-                    })}
-                </div>
-                {image.link ? (
-                  <div className="link">
-                    <Link target="_blank" to={image.link}>
-                      {image.link}
-                    </Link>
-                  </div>
-                ) : null}
-                <div className="grayRight">
-                  {image.time ? (
-                    <div className="time">
-                      {image.time.toLocaleString("ja", opt)}
+            {isComplete ? (
+              <>
+                {editMode ? null : (
+                  <div className="info window">
+                    {image.album.visible.title &&
+                    (image.album.visible.filename || !titleEqFilename) ? (
+                      <h2 className="title">{image.name}</h2>
+                    ) : (
+                      <div className="title" />
+                    )}
+                    <div className="description">
+                      <MultiParser>{image.description}</MultiParser>
                     </div>
-                  ) : null}
-                  {image.type === "ebook" ? (
-                    <div>本のマークから読むことができる作品です！</div>
-                  ) : null}
+                    <div className="tagList">
+                      {charaList
+                        .filter((chara) =>
+                          image.tags?.some((tag) => chara.id === tag)
+                        )
+                        .map((chara, i) => {
+                          return (
+                            <Link
+                              to={"/character/" + chara.id}
+                              onClick={() => {
+                                onClose();
+                                return true;
+                              }}
+                              className="character"
+                              key={i}
+                            >
+                              {chara?.media?.icon ? (
+                                <ImageMee
+                                  imageItem={chara.media.icon}
+                                  mode="icon"
+                                  width={40}
+                                  height={40}
+                                  className="charaIcon"
+                                />
+                              ) : (
+                                <></>
+                              )}
+                              <span className="align-middle">{chara.name}</span>
+                            </Link>
+                          );
+                        })}
+                      {image.tags
+                        ?.filter((tag) =>
+                          tagsOptions.some(({ value }) => value === tag)
+                        )
+                        .map((tag, i) => {
+                          const item = tagsOptions.find(
+                            ({ value }) => value === tag
+                          );
+                          if (!item) return item;
+                          return (
+                            <Link
+                              to={MakeRelativeURL({
+                                ...(pathname.startsWith("/gallery")
+                                  ? {}
+                                  : { pathname: "/gallery" }),
+                                query: item.query || { tag: item.value },
+                              })}
+                              className="other"
+                              preventScrollReset={false}
+                              key={i}
+                            >
+                              <span>{item.label}</span>
+                            </Link>
+                          );
+                        })}
+                    </div>
+                    {image.link ? (
+                      <div className="link">
+                        <Link target="_blank" to={image.link}>
+                          {image.link}
+                        </Link>
+                      </div>
+                    ) : null}
+                    <div className="grayRight">
+                      {image.time ? (
+                        <div className="time">
+                          {image.time.toLocaleString("ja", opt)}
+                        </div>
+                      ) : null}
+                      {image.type === "ebook" ? (
+                        <div>本のマークから読むことができる作品です！</div>
+                      ) : null}
+                    </div>
+                    {children}
+                  </div>
+                )}
+                {isDev ? <ImageEditForm /> : null}
+              </>
+            ) : null}
+            <div className="paging">
+              {beforeAfterImage?.before ? (
+                <Link
+                  className="prev"
+                  to={MakeRelativeURL({
+                    query: {
+                      ...Object.fromEntries(search),
+                      image: beforeAfterImage.before.originName,
+                      ...(beforeAfterImage.before.album?.name
+                        ? { album: beforeAfterImage.before.album.name }
+                        : {}),
+                    },
+                  })}
+                  preventScrollReset={false}
+                  replace={true}
+                >
+                  <div className="cursor">≪</div>
+                  <div>{beforeAfterImage.before.name}</div>
+                </Link>
+              ) : (
+                <div className="flex-1" />
+              )}
+              {beforeAfterImage?.after ? (
+                <Link
+                  className="next"
+                  to={MakeRelativeURL({
+                    query: {
+                      ...Object.fromEntries(search),
+                      image: beforeAfterImage.after.originName,
+                      ...(beforeAfterImage.after.album?.name
+                        ? { album: beforeAfterImage.after.album.name }
+                        : {}),
+                    },
+                  })}
+                  preventScrollReset={false}
+                  replace={true}
+                >
+                  <div>{beforeAfterImage.after.name}</div>
+                  <div className="cursor">≫</div>
+                </Link>
+              ) : (
+                <div className="flex-1" />
+              )}
+            </div>
+          </>
+        ) : null}
+      </div>
+    ),
+    [
+      beforeAfterImage,
+      charaList,
+      editMode,
+      image,
+      isDev,
+      onClose,
+      pathname,
+      search,
+      tagsOptions,
+      titleEqFilename,
+      query,
+      isComplete,
+    ]
+  );
+
+  const PreviewArea = useCallback(
+    () => (
+      <div className="preview">
+        {image ? (
+          <>
+            {image.embed && (image.type === "embed" || image.type === "3d") ? (
+              <>
+                <EmbedNode className="wh-all-fill" embed={image.embed} />
+              </>
+            ) : (
+              <div className="wh-fill">
+                <a
+                  title="別タブで画像を開く"
+                  href={(isProd ? image.URL : image.origin) || image.src}
+                  target="_blank"
+                  className="fullscreen-button"
+                >
+                  <RiFullscreenFill />
+                </a>
+                {image.embed ? (
+                  image.type === "ebook" ? (
+                    <Link
+                      title="よむ"
+                      to={MakeRelativeURL({
+                        pathname: "/gallery",
+                        query: { ebook: image.embed },
+                      })}
+                      className="read-button"
+                    >
+                      <RiBook2Fill />
+                    </Link>
+                  ) : image.type === "pdf" ? (
+                    <a
+                      title="ひらく"
+                      href={getEmbedURL(image.embed)}
+                      target="_blank"
+                      className="read-button"
+                    >
+                      <RiFilePdf2Fill />
+                    </a>
+                  ) : null
+                ) : null}
+                <div className="wh-all-fill imageArea">
+                  <ImageMee imageItem={image} title={image.name || image.src} />
                 </div>
               </div>
             )}
-            {isDev ? <ImageEditForm /> : null}
           </>
         ) : null}
-        <div className="paging">
-          {beforeAfterImage?.before ? (
-            <Link
-              className="prev"
-              to={MakeRelativeURL({
-                query: {
-                  ...Object.fromEntries(search),
-                  image: beforeAfterImage.before.originName,
-                  ...(beforeAfterImage.before.album?.name
-                    ? { album: beforeAfterImage.before.album.name }
-                    : {}),
-                },
-              })}
-              preventScrollReset={false}
-              replace={true}
-            >
-              <div className="cursor">≪</div>
-              <div>{beforeAfterImage.before.name}</div>
-            </Link>
-          ) : (
-            <div className="flex-1" />
-          )}
-          {beforeAfterImage?.after ? (
-            <Link
-              className="next"
-              to={MakeRelativeURL({
-                query: {
-                  ...Object.fromEntries(search),
-                  image: beforeAfterImage.after.originName,
-                  ...(beforeAfterImage.after.album?.name
-                    ? { album: beforeAfterImage.after.album.name }
-                    : {}),
-                },
-              })}
-              preventScrollReset={false}
-              replace={true}
-            >
-              <div>{beforeAfterImage.after.name}</div>
-              <div className="cursor">≫</div>
-            </Link>
-          ) : (
-            <div className="flex-1" />
-          )}
-        </div>
       </div>
-    );
-  }, [
-    beforeAfterImage,
-    charaList,
-    editMode,
-    image,
-    isDev,
-    onClose,
-    pathname,
-    search,
-    tagsOptions,
-    titleEqFilename,
-    query,
-    isComplete,
-  ]);
-
-  const previewArea = useMemo(() => {
-    if (!image) return <></>;
-    return (
-      <>
-        {image.embed && (image.type === "embed" || image.type === "3d") ? (
-          <>
-            <EmbedNode className="wh-all-fill" embed={image.embed} />
-          </>
-        ) : (
-          <div className="wh-fill">
-            <a
-              title="別タブで画像を開く"
-              href={(isProd ? image.URL : image.origin) || image.src}
-              target="_blank"
-              className="fullscreen-button"
-            >
-              <RiFullscreenFill />
-            </a>
-            {image.embed ? (
-              image.type === "ebook" ? (
-                <Link
-                  title="よむ"
-                  to={MakeRelativeURL({
-                    pathname: "/gallery",
-                    query: { ebook: image.embed },
-                  })}
-                  className="read-button"
-                >
-                  <RiBook2Fill />
-                </Link>
-              ) : image.type === "pdf" ? (
-                <a
-                  title="ひらく"
-                  href={getEmbedURL(image.embed)}
-                  target="_blank"
-                  className="read-button"
-                >
-                  <RiFilePdf2Fill />
-                </a>
-              ) : null
-            ) : null}
-            <div className="wh-all-fill imageArea">
-              <ImageMee imageItem={image} title={image.name || image.src} />
-            </div>
-          </div>
-        )}
-      </>
-    );
-  }, [image, isProd]);
+    ),
+    [image, isProd]
+  );
 
   return (
     <div id="image_viewer">
@@ -422,8 +405,8 @@ export default function ImageViewer() {
               }}
             />
             <div className="window modal">
-              <div className="preview">{previewArea}</div>
-              {InfoCmp}
+              <PreviewArea />
+              <InfoCmp />
             </div>
           </div>
         </div>
