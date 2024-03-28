9ab662d38984003993045602c9592576d0dde276
kouwtkz <kouwtkz@cottonwind.com>
2024-03-28 15:00:46 +0900

    保存時に自動的にJSONにする拡張を導入した

--- src/mediaScripts/DataUpdate.mjs
+++ src/mediaScripts/DataUpdate.mjs
@@ -11,20 +11,41 @@ import { getSoundAlbum } from "../data/functions/FunctionsSoundData.mjs";
 import { getCharaObjectFromYaml } from "../data/functions/FunctionsCharaData.mjs";
 import { getSiteData } from "../data/functions/FunctionsSiteData.mjs";
 
+const mode = process.argv[2] ?? null;
+/** @type string[] */
+const doneList = [];
+
+// サイトデータのコピー
+if (!mode || mode === "site") {
+  exportTsOut("site", getSiteData());
+  doneList.push("サイト");
+}
+
+// キャラクターデータのコピー
+if (!mode || mode === "character") {
+  exportJsonOut("characters", getCharaObjectFromYaml());
+  doneList.push("キャラクター");
+}
+
 // 画像データのコピー
-const albumData = await GetMediaImageAlbums({ ...fromto, readSize: true, filter: { archive: false }, readImageHandle: ReadImageFromYamls });
-exportJsonOut("images", albumData);
-await UpdateImageYaml({ ...fromto, retouchImageHandle: retouchImageFromYamls });
+if (!mode || mode === "image") {
+  const albumData = await GetMediaImageAlbums({ ...fromto, readSize: true, filter: { archive: false }, readImageHandle: ReadImageFromYamls });
+  exportJsonOut("images", albumData);
+  await UpdateImageYaml({ ...fromto, retouchImageHandle: retouchImageFromYamls });
+  doneList.push("画像");
+}
 
 // 埋め込みや電子書籍ファイルのコピー
-CopyDirDiff("_data/embed", "public/static", { identical: true });
+if (!mode || mode === "embed") {
+  CopyDirDiff("_data/embed", "public/static", { identical: true });
+  doneList.push("埋め込み");
+}
 
 // 音楽ファイルのコピー
-CopyDirDiff("_data/sound", "public/static", { identical: true, ignore: "_data.yaml" });
-exportJsonOut("sound", getSoundAlbum());
+if (!mode || mode === "sound") {
+  CopyDirDiff("_data/sound", "public/static", { identical: true, ignore: "_data.yaml" });
+  exportJsonOut("sound", getSoundAlbum());
+  doneList.push("サウンド");
+}
 
-exportJsonOut("characters", getCharaObjectFromYaml());
-
-exportTsOut("site", getSiteData());
-
-console.log("メディアの更新しました");
+console.log({ message: "データの更新しました", "更新内容": doneList });
