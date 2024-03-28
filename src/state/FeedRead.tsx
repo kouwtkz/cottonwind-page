4c4092e20851ef30bfe69e03c48bc5e87bd0381a
kouwtkz <kouwtkz@cottonwind.com>
2024-03-28 15:04:23 +0900

    RSSでタイトルがないものにも対応した

--- src/state/FeedRead.tsx
+++ src/state/FeedRead.tsx
@@ -3,6 +3,7 @@ import { create } from "zustand";
 
 type FeedArticleType = {
   title: string;
+  description: string;
   link: string;
   date: Date;
 };
@@ -13,33 +14,40 @@ type FeedStateType = {
   description?: string;
   list?: FeedArticleType[];
   isSet: boolean;
+  isFirst: boolean;
   set: () => void;
 };
 
 export const useFeedState = create<FeedStateType>((set) => ({
   isSet: false,
+  isFirst: true,
   set: () => {
     fetch("/get/rss")
       .then((res) => {
-        return res.text();
+        return res.headers.get("Content-Type")?.startsWith("application/xml")
+          ? res.text()
+          : "";
       })
       .then((text) => {
-        const p = new DOMParser();
-        const dom = p.parseFromString(text, "application/xml");
-        const title = dom.querySelector("title")?.textContent!;
-        const link = dom.querySelector("link")?.textContent!;
-        const description = dom.querySelector("description")?.textContent!;
-        const articles = Array.from(
-          dom.querySelectorAll("item:nth-of-type(-n+20)")
-        );
-        const list = articles.map((item) => {
-          return {
-            title: item.querySelector("title")?.textContent!,
-            link: item.querySelector("link")?.textContent!,
-            date: new Date(item.querySelector("pubDate")?.textContent!),
-          };
-        });
-        set({ title, link, description, list, isSet: true });
+        if (text) {
+          const p = new DOMParser();
+          const dom = p.parseFromString(text, "application/xml");
+          const title = dom.querySelector("title")?.textContent!;
+          const link = dom.querySelector("link")?.textContent!;
+          const description = dom.querySelector("description")?.textContent!;
+          const articles = Array.from(
+            dom.querySelectorAll("item:nth-of-type(-n+20)")
+          );
+          const list = articles.map((item) => {
+            return {
+              title: item.querySelector("title")?.textContent!,
+              description: item.querySelector("description")?.textContent!,
+              link: item.querySelector("link")?.textContent!,
+              date: new Date(item.querySelector("pubDate")?.textContent!),
+            };
+          });
+          set({ title, link, description, list, isSet: true, isFirst: false });
+        } else set({ isFirst: false });
       });
   },
 }));
@@ -50,20 +58,21 @@ export function FeedState() {
 }
 
 export const FeedRead = memo(function FeedRead() {
-  const { isSet, title, link, set, list } = useFeedState();
+  const { isSet, isFirst, title, link, set, list } = useFeedState();
   useLayoutEffect(() => {
-    if (!isSet) set();
-  }, [isSet]);
+    if (isFirst) set();
+  }, [isFirst]);
+  if (!isSet) return <></>;
   return (
     <div className="blog">
       <a className="title" href={link} title={title} target="blog">
-        <h3 className="text-3xl">ブログ</h3>
+        <h3 className="text-3xl">ノート</h3>
       </a>
       <div className="list">
-        {list?.slice(0, 3).map(({ date, link, title }, i) => (
-          <a href={link} className="article" target="blog" key={i}>
+        {list?.slice(0, 3).map(({ date, link, title, description }, i) => (
+          <a href={link} className="article" target="note" key={i}>
             <div className="date">{date.toLocaleDateString("ja")}</div>
-            <div className="title">{title}</div>
+            <div className="title">{title || description.slice(0, 32)}</div>
           </a>
         ))}
       </div>
