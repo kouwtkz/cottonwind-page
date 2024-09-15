e3c493aafca107e2c05568845d6a7d9ac125608b
kouwtkz <kouwtkz@cottonwind.com>
2024-09-15 17:07:10 +0900

    ログイン判定関数を扱いやすくした

--- src/ServerContent.ts
+++ src/ServerContent.ts
@@ -8,8 +8,9 @@ export const XmlHeader = {
   headers: { "Content-Type": "application/xml; charset=UTF-8" },
 }
 
-export function IsLogin<T extends MeeCommonEnv>(c: CommonContext<T>) {
-  return Boolean(c.env.DEV) || (c.env?.LOGIN_TOKEN === getCookie(c, "LoginToken"))
+export function IsLogin<T extends MeeCommonEnv>(c: CommonContext<T>, trueWhenDev = true) {
+  if (trueWhenDev && c.env.DEV) return Boolean(c.env.DEV);
+  return c.env?.LOGIN_TOKEN === getCookie(c, "LoginToken");
 }
 
 export async function FetchBody(src?: string) {
