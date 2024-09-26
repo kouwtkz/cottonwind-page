6dc3c4a2651e864a1bde9481472b478457a8cd4c
kouwtkz <kouwtkz@cottonwind.com>
2024-09-27 00:05:21 +0900

    fixSourceMapsの役割がわからなくなってしまっているのとビルドの妨げになっていたので廃止した

--- vite.config.ts
+++ vite.config.ts
@@ -2,50 +2,13 @@ import pages from '@hono/vite-cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import { configDotenv } from 'dotenv'
-import { Plugin, UserConfig, defineConfig } from 'vite'
+import { UserConfig, defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import Sitemap from "vite-plugin-sitemap";
 import { RoutingList } from './src/routes/RoutingList';
 import { getPlatformProxy } from 'wrangler'
 const { env, dispose } = await getPlatformProxy<MeeCommonEnv>();
 dispose();
-
-import fs from 'node:fs';
-import path from 'node:path';
-const __dirname = import.meta.dirname;
-
-function fixSourceMaps(): Plugin {
-  let currentInterval: NodeJS.Timeout | null = null
-  return {
-    name: 'fix-source-map',
-    enforce: 'post',
-    transform: function (source: any) {
-      if (currentInterval) {
-        return;
-      }
-      currentInterval = setInterval(function () {
-        const nodeModulesPath = path.join(__dirname, 'node_modules', '.vite', 'deps');
-        if (fs.existsSync(nodeModulesPath)) {
-          clearInterval(currentInterval!);
-          currentInterval = null;
-          const files = fs.readdirSync(nodeModulesPath);
-          files.forEach(function (file) {
-            const mapFile = file + '.map';
-            const mapPath = path.join(nodeModulesPath, mapFile);
-            if (fs.existsSync(mapPath)) {
-              let mapData = JSON.parse(fs.readFileSync(mapPath, 'utf8'));
-              if (!mapData.sources || mapData.sources.length == 0) {
-                mapData.sources = [path.relative(mapPath, path.join(nodeModulesPath, file))];
-                fs.writeFileSync(mapPath, JSON.stringify(mapData), 'utf8');
-              }
-            }
-          });
-        }
-      }, 100);
-      return source;
-    }
-  }
-}
 
 export default defineConfig(({ mode }) => {
   let config: UserConfig = {
@@ -100,7 +63,6 @@ export default defineConfig(({ mode }) => {
       config.ssr = { external: ['axios', 'react', 'react-dom', 'xmldom', 'xpath', 'tsqlstring'] };
       config.plugins!.push([
         pages(),
-        fixSourceMaps(),
         devServer({
           entry: 'src/index.dev.tsx',
           adapter,
