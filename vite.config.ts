9e178565a5ef8113536d7334505ffbe69f64b379
kouwtkz <kouwtkz@cottonwind.com>
2024-09-27 23:50:27 +0900

    vite.configやビルド周りをわかりやすくしたのとディレクトリを上書きするかどうかのオプションを設けた

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-import pages from '@hono/vite-cloudflare-pages'
-import devServer from '@hono/vite-dev-server'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import { configDotenv } from 'dotenv'
-import { UserConfig, defineConfig } from 'vite'
+import pages from '@hono/vite-cloudflare-pages';
+import devServer from '@hono/vite-dev-server';
+import adapter from '@hono/vite-dev-server/cloudflare';
+import { PluginOption, UserConfig, defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import Sitemap from "vite-plugin-sitemap";
 import { RoutingList } from './src/routes/RoutingList';
-import { getPlatformProxy } from 'wrangler'
-import buildMeeSSG_Plugins from './buildMeeSSG_Plugins'
+import { getPlatformProxy } from 'wrangler';
+import buildMeeSSG_Plugins from './buildMeeSSG_Plugins';
 
 export default defineConfig(async ({ mode }) => {
+  let defaultPlugins: PluginOption[] = [tsconfigPaths()];
   let config: UserConfig = {
     optimizeDeps: { include: [] },
-    plugins: [tsconfigPaths()],
     css: {
       preprocessorOptions: {
         scss: {
@@ -21,72 +20,87 @@ export default defineConfig(async ({ mode }) => {
       }
     }
   };
-  if (mode === "client") {
-    config.build = {
-      rollupOptions: {
-        input: [
-          './src/client.tsx',
-          './src/styles.scss',
-          './src/styles-import.scss',
-          './src/workers/twix/twixClient.tsx'
-        ],
-        output: {
-          entryFileNames: `static/js/[name].js`,
-          chunkFileNames: `static/js/[name].js`,
-          assetFileNames: (assetInfo) => {
-            const name = assetInfo?.name ?? "";
-            if (/\.(gif|jpeg|jpg|png|svg|webp)$/.test(name)) {
-              return 'static/images/[name].[ext]';
+  const modes = mode.split("-");
+  if (modes[0] === "client") {
+    return {
+      ...config,
+      plugins: defaultPlugins,
+      build: {
+        emptyOutDir: modes[1] === "overwrite",
+        rollupOptions: {
+          input: [
+            './src/client.tsx',
+            './src/styles.scss',
+            './src/styles-import.scss',
+            './src/workers/twix/twixClient.tsx'
+          ],
+          output: {
+            entryFileNames: `static/js/[name].js`,
+            chunkFileNames: `static/js/[name].js`,
+            assetFileNames: (assetInfo) => {
+              const name = assetInfo?.name ?? "";
+              if (/\.(gif|jpeg|jpg|png|svg|webp)$/.test(name)) {
+                return 'static/images/[name].[ext]';
+              }
+              if (/\.css$/.test(name)) {
+                return 'css/[name].[ext]';
+              }
+              return 'static/[name].[ext]';
             }
-            if (/\.css$/.test(name)) {
-              return 'css/[name].[ext]';
+          },
+          onwarn(warning, warn) {
+            // Suppress "Module level directives cause errors when bundled" warnings
+            if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+              return;
             }
-            return 'static/[name].[ext]';
-          }
+            warn(warning);
+          },
         },
-        onwarn(warning, warn) {
-          // Suppress "Module level directives cause errors when bundled" warnings
-          if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-            return;
-          }
-          warn(warning);
-        },
-      },
-      // manifest: true,
-      chunkSizeWarningLimit: 3000
-    }
-  } else if (mode === "ssg") {
+        // manifest: true,
+        chunkSizeWarningLimit: 3000
+      }
+    } as UserConfig;
+  } else if (modes[0] === "ssg") {
     const { env, dispose } = await getPlatformProxy<MeeCommonEnv>();
     const entry = "src/ssg.tsx";
-    config.plugins!.push([
-      buildMeeSSG_Plugins({ entry, adapter: { env, onServerClose: dispose } }),
-      Sitemap({
-        hostname: env.ORIGIN,
-        generateRobotsTxt: true,
-        dynamicRoutes: RoutingList.filter(v => !/:/.test(v)),
-        exclude: ["/404", "/500", "/suggest"]
-      }),
-    ]);
+    return {
+      ...config,
+      plugins: [
+        ...defaultPlugins,
+        buildMeeSSG_Plugins({ entry, adapter: { env, onServerClose: dispose } }),
+        Sitemap({
+          hostname: env.ORIGIN,
+          generateRobotsTxt: true,
+          dynamicRoutes: RoutingList.filter(v => !/:/.test(v)),
+          exclude: ["/404", "/500", "/suggest"]
+        }),
+      ],
+      build: {
+        emptyOutDir: modes[1] === "overwrite"
+      }
+    } as UserConfig;
   } else {
-    configDotenv();
-    config.ssr = { external: ['axios', 'react', 'react-dom', 'xmldom', 'xpath', 'tsqlstring'] };
-    config.plugins!.push([
-      pages(),
-      devServer({
-        entry: 'src/index.dev.tsx',
-        adapter,
-        exclude: [
-          // /.*\.css$/,
-          /.*\.ts$/,
-          /.*\.tsx$/,
-          /^\/@.+$/,
-          /\?t\=\d+$/,
-          /^\/favicon\.ico$/,
-          /^\/static\/.+/,
-          /^\/node_modules\/.*/,
-        ],
-      }),
-    ])
+    return {
+      ...config,
+      ssr: { external: ['axios', 'react', 'react-dom', 'xmldom', 'xpath', 'tsqlstring'] },
+      plugins: [
+        ...defaultPlugins,
+        pages(),
+        devServer({
+          entry: 'src/index.dev.tsx',
+          adapter,
+          exclude: [
+            // /.*\.css$/,
+            /.*\.ts$/,
+            /.*\.tsx$/,
+            /^\/@.+$/,
+            /\?t\=\d+$/,
+            /^\/favicon\.ico$/,
+            /^\/static\/.+/,
+            /^\/node_modules\/.*/,
+          ],
+        }),
+      ],
+    } as UserConfig;
   }
-  return config;
 })
