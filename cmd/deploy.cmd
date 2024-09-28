304673850af458cafdc6a9749951c4cdc5249f06
kouwtkz <kouwtkz@cottonwind.com>
2024-09-28 23:19:11 +0900

    よく使う必要がなくなったcmdの各コマンドを削除した

--- cmd/deploy.cmd
+++
@@ -1,3 +0,0 @@
-call npm run build
-call npm run deploy
-pause
--- cmd/redirect/deploy_cloudflare.cmd
+++
@@ -1,2 +0,0 @@
-call wrangler pages deploy cf_redirect --project-name redirect-kouwtkz-cottonwind --commit-dirty true --branch master
-pause
