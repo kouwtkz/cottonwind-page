95de3a3bcfda2c770c80989c7fca5ffc050a3f21
kouwtkz <kouwtkz@cottonwind.com>
2024-10-06 00:55:31 +0900

    Pagesの履歴は毎朝4時に一昨日以前を削除する仕様にした

--- src/sv/index.ts
+++ src/sv/index.ts
@@ -1,9 +1,24 @@
 import { Hono } from "hono";
-import { scheduleTask } from "./schedule";
-export const app = new Hono<MeeBindings<MeeCommonEnv>>();
-const scheduled: ExportedHandlerScheduledHandler<MeeCommonEnv> = async (event, env, ctx) => {
+import { SvCleanPages, SvLifeCheck, scheduleTask } from "./schedule";
+export const app = new Hono<MeeBindings<MeeSvEnv>>();
+const scheduled: ExportedHandlerScheduledHandler<MeeSvEnv> = async (event, env, ctx) => {
   ctx.waitUntil(scheduleTask(event, env));
 };
+
+app.get("/life", async (c, next) => {
+  if (!c.env.DEV) return next();
+  await SvLifeCheck(c.env);
+  return c.text("");
+})
+
+app.get("/clean", async (c, next) => {
+  if (!c.env.DEV) return next();
+  const Url = new URL(c.req.url);
+  const day = Url.searchParams.has("d") ? Number(Url.searchParams.get("d")) : null;
+  await SvCleanPages(c.env, day && !isNaN(day) ? day : undefined);
+  return c.text("");
+})
+
 export default {
   fetch: app.fetch,
   scheduled,
--- src/sv/schedule.ts
+++ src/sv/schedule.ts
@@ -1,49 +1,55 @@
 export async function scheduleTask(event: ScheduledController, env: MeeSvEnv) {
 	switch (event.cron) {
 		case "0 19 * * *":
-			if (env.LIFE_CHECK_URL) {
-				const key = "life-check";
-				const url = env.LIFE_CHECK_URL;
-				const last = await env.KV.get(key);
-				const result = String(
-					await fetch(url, { method: "POST", body: env.LIFE_CHECK_CHALLENGE })
-						.then(async (r) => {
-							return await r.text() === env.LIFE_CHECK_VERIFIER
-						}) ?? false
-				);
-				if (last !== result) await env.KV.put(key, result);
-			}
-			break;
-		case "0 15 * * tue":
-			if (!env.ACCOUNT_ID || !env.PROJECT_NAME || !env.WORKERS_API_TOKEN) break;
-			const cronNum = Number(event.cron);
-			const expirationDays = isNaN(cronNum) ? (env.EXPIRATION_DAYS || 7) : cronNum;
-			const endpoint = `https://api.cloudflare.com/client/v4/accounts/${env.ACCOUNT_ID}/pages/projects/${env.PROJECT_NAME}/deployments`;
-			const init = {
-				headers: {
-					"Content-Type": "application/json;charset=UTF-8",
-					// We recommend you store the API token as a secret using the Workers dashboard or using Wrangler as documented here: https://developers.cloudflare.com/workers/wrangler/commands/#secret
-					"Authorization": `Bearer ${env.API_TOKEN}`,
-				},
-			};
-
-			const response = await fetch(endpoint, init);
-			const deployments = await response.json() as { [k: string]: any };
-			const resultList = deployments.result as any[];
-			await Promise.all(resultList.map(async (deployment) => {
-				if (deployment.aliases) return;
-				// Check if the deployment was created within the last x days (as defined by `expirationDays` above)
-				if ((Date.now() - new Date(deployment.created_on).getTime()) > (expirationDays * 86400000)) {
-					// Delete the deployment
-					await fetch(`${endpoint}/${deployment.id}`, {
-						method: "DELETE",
-						headers: {
-							"Content-Type": "application/json;charset=UTF-8",
-							"Authorization": `Bearer ${env.API_TOKEN}`,
-						},
-					});
-				}
-			}));
+			if (env.DEV) console.log(event.cron);
+			await SvLifeCheck(env);
+			await SvCleanPages(env);
 			break;
 	}
+}
+
+export async function SvLifeCheck(env: MeeSvEnv) {
+	if (env.LIFE_CHECK_URL) {
+		const key = "life-check";
+		const url = env.LIFE_CHECK_URL;
+		const last = await env.KV.get(key);
+		const result = String(
+			await fetch(url, { method: "POST", body: env.LIFE_CHECK_CHALLENGE })
+				.then(async (r) => {
+					return await r.text() === env.LIFE_CHECK_VERIFIER
+				}) ?? false
+		);
+		if (last !== result) await env.KV.put(key, result);
+	}
+}
+
+export async function SvCleanPages(env: MeeSvEnv, expirationDays?: number) {
+	if (!env.ACCOUNT_ID || !env.PROJECT_NAME || !env.WORKERS_API_TOKEN) return;
+	if (expirationDays === undefined) expirationDays = env.EXPIRATION_DAYS || 1;
+	const endpoint = `https://api.cloudflare.com/client/v4/accounts/${env.ACCOUNT_ID}/pages/projects/${env.PROJECT_NAME}/deployments`;
+	const init = {
+		headers: {
+			"Content-Type": "application/json;charset=UTF-8",
+			// We recommend you store the API token as a secret using the Workers dashboard or using Wrangler as documented here: https://developers.cloudflare.com/workers/wrangler/commands/#secret
+			"Authorization": `Bearer ${env.WORKERS_API_TOKEN}`,
+		},
+	};
+	const response = await fetch(endpoint, init);
+	const deployments = await response.json() as { [k: string]: any };
+	const resultList = (deployments.result || []) as any[];
+	const now = Date.now();
+	await Promise.all(resultList.map(async (deployment) => {
+		if (deployment.aliases) return;
+		// Check if the deployment was created within the last x days (as defined by `expirationDays` above)
+		if ((now - new Date(deployment.created_on).getTime()) > (expirationDays * 86400000)) {
+			// Delete the deployment
+			await fetch(`${endpoint}/${deployment.id}`, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json;charset=UTF-8",
+					"Authorization": `Bearer ${env.WORKERS_API_TOKEN}`,
+				},
+			});
+		}
+	}));
 }
