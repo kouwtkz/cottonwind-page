63aff6cc819826457e905811d6e417b1ecf0395d
kouwtkz <kouwtkz@cottonwind.com>
2025-04-19 05:38:56 +0900

    サービスワーカー周りのファイル名の見直し

--- src/components/serviceWorker/swDefault.ts
+++ src/components/serviceWorker/swDefault.ts
@@ -1,21 +1,15 @@
 namespace SW {
   declare const self: ServiceWorkerGlobalScope;
-
-  // self.registration.update()
-  self.addEventListener('fetch', function (e) {
-    const event = e as FetchEvent;
-  })
-
-  function FetchCache(event: FetchEvent) {
-    event.respondWith(
-      (async () => {
-        // // キャッシュからレスポンスを取得しようとします。
-        const cachedResponse = await caches.match(event.request);
-        // // 見つかったらそれを返します。
-        if (cachedResponse) return cachedResponse;
-        // キャッシュ内に一致するものが見つからなかった場合は、ネットワークを使用します。
-        return fetch(event.request);
-      })(),
-    );
-  }
+  addEventListener("message", (event) => {
+    switch (typeof event.data) {
+      case "string":
+        if (event.data.startsWith("{")) {
+          const data = JSON.parse(event.data);
+          if (data.notification) {
+            self.registration.showNotification(data.notification)
+          }
+        }
+        break;
+    }
+  });
 }
