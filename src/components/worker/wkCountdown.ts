b601bb2fa5e9e45d0d3bfeb632d90372d094f75c
kouwtkz <kouwtkz@cottonwind.com>
2025-04-20 13:18:37 +0900

    カウントダウンがほぼ正確になるように1秒ごとに日付を比較してカウントするようにした

--- src/components/worker/wkCountdown.ts
+++ src/components/worker/wkCountdown.ts
@@ -2,7 +2,6 @@ import { dataParse } from "@/functions/dataParse";
 
 namespace WK {
   let countdownId: NodeJS.Timeout | null = null;
-  let countdownTime = 0;
   addEventListener("message", (e) => {
     function Post<T = WkSendDataType>(message: T, options?: WindowPostMessageOptions) {
       postMessage(JSON.stringify(message), options);
@@ -17,9 +16,13 @@ namespace WK {
         clearInterval(countdownId);
       }
       if (data.setCountdown) {
-        countdownTime = data.setCountdown;
+        let countdownTime = data.setCountdown;
+        const startDate = new Date();
+        const endDate = new Date(startDate);
+        endDate.setSeconds(endDate.getSeconds() + countdownTime);
         countdownId = setInterval(() => {
-          countdownTime--;
+          const currentDate = new Date();
+          countdownTime = Math.round((endDate.getTime() - currentDate.getTime()) / 1000);
           Post({ countdownTime });
           if (countdownTime < 0 && countdownId) { clearInterval(countdownId); }
         }, 1000);
