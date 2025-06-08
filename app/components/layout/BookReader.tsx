f2674af8766e65a7c3e361ab090312970fdd4cfe
kouwtkz <kouwtkz@cottonwind.com>
2025-06-08 15:53:11 +0900

    laymicのサムネイル一覧でスクロールができなくなっていたのを直した

--- app/components/layout/BookReader.tsx
+++ app/components/layout/BookReader.tsx
@@ -76,10 +76,18 @@ export function BookReader() {
   }, [url, isHashLaymic]);
   const laymicContent = useMemo(() => {
     if (srcList) {
-      console.log(metadata);
       const laymicContent = new laymic.Laymic(
         { pages: srcList, thumbs: [] },
-        { isInstantOpen: false, isLTR: metadata?.direction === "ltr" }
+        {
+          isInstantOpen: false,
+          isLTR: metadata?.direction === "ltr",
+          classNames: {
+            thumbs: {
+              ...defaultLaymicClassNames.thumbs,
+              wrapper: "laymic_thumbsWrapper window",
+            },
+          },
+        }
       );
       return laymicContent;
     } else return null;
@@ -129,3 +137,88 @@ export function BookReader() {
     </>
   );
 }
+
+const defaultLaymicClassNames = {
+  root: "laymic_root",
+  slider: "laymic_slider",
+  // uiボタンクラス名
+  uiButton: "laymic_uiButton",
+  // 空スライドクラス名
+  emptySlide: "laymic_emptySlide",
+  pagination: "laymic_pagination",
+  controller: {
+    controller: "laymic_controller",
+    controllerTop: "laymic_controllerTop",
+    controllerBottom: "laymic_controllerBottom",
+  },
+  buttons: {
+    direction: "laymic_direction",
+    fullscreen: "laymic_fullscreen",
+    thumbs: "laymic_showThumbs",
+    preference: "laymic_showPreference",
+    close: "laymic_close",
+    help: "laymic_showHelp",
+    nextPage: "laymic_paginationNext",
+    prevPage: "laymic_paginationPrev",
+    zoom: "laymic_zoom",
+    progressbar: "laymic_progressbar",
+  },
+  svg: {
+    icon: "laymic_svgIcon",
+    defaultProp: "laymic_svgDefaultProp",
+    container: "laymic_svgContainer",
+  },
+  checkbox: {
+    container: "laymic_checkbox",
+    label: "laymic_checkboxLabel",
+    iconWrapper: "laymic_checkboxIconWrapper",
+  },
+  select: {
+    container: "laymic_select",
+    label: "laymic_selectLabel",
+    wrapper: "laymic_selectWrapper",
+    current: "laymic_selectCurrentItem",
+    item: "laymic_selectItem",
+    itemWrapper: "laymic_selectItemWrapper",
+  },
+  thumbs: {
+    container: "laymic_thumbs",
+    wrapper: "laymic_thumbsWrapper",
+    item: "laymic_thumbItem",
+    slideThumb: "laymic_slideThumb",
+    imgThumb: "laymic_imgThumb",
+    lazyload: "laymic_lazyload",
+    lazyloading: "laymic_lazyloading",
+    lazyloaded: "laymic_lazyloaded",
+  },
+  preference: {
+    container: "laymic_preference",
+    wrapper: "laymic_preferenceWrapper",
+    notice: "laymic_preferenceNotice",
+    button: "laymic_preferenceButton",
+    paginationVisibility: "laymic_preferencePaginationVisibility",
+    isAutoFullscreen: "laymic_preferenceIsAutoFullscreen",
+    zoomButtonRatio: "laymic_preferenceZoomButtonRatio",
+    isDisabledTapSlidePage: "laymic_preferenceIsDisabledTapSlidePage",
+    isDisabledForceHorizView: "laymic_preferenceIsDisabledForceHorizView",
+    isDisabledDoubleTapResetZoom:
+      "laymic_preferenceIsDisabledDoubleTapResetZoom",
+  },
+  help: {
+    container: "laymic_help",
+    wrapper: "laymic_helpWrapper",
+    vertImg: "laymic_helpVertImg",
+    horizImg: "laymic_helpHorizImg",
+    innerWrapper: "laymic_helpInnerWrapper",
+    innerItem: "laymic_helpInnerItem",
+    iconWrapper: "laymic_helpIconWrapper",
+    iconLabel: "laymic_helpIconLabel",
+    chevronsContainer: "laymic_helpChevrons",
+    zoomItem: "laymic_helpZoomItem",
+    fullscreenItem: "laymic_helpFullscreenItem",
+  },
+  zoom: {
+    controller: "laymic_zoomController",
+    wrapper: "laymic_zoomWrapper",
+  },
+};
