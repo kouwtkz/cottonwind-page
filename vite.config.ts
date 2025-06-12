af863891bf0fc5099c9f22a4b1e240ca3237b4e1
kouwtkz <kouwtkz@cottonwind.com>
2025-06-12 18:08:29 +0900

    環境設定の調整

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -12,7 +11,6 @@ export default defineConfig({
   },
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
-    tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
   ],
