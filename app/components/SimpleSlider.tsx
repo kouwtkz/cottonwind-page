4489d74d3861e9ba17142a01fccc93ea9052c57d
kouwtkz <kouwtkz@cottonwind.com>
2025-06-12 10:51:27 +0900

    読み込み中で使うスライダーでmaxが0の場合、NaNになっていたのを直した

--- app/components/SimpleSlider.tsx
+++ app/components/SimpleSlider.tsx
@@ -1,14 +1,14 @@
+import { useMemo } from "react";
+
 interface SimpleSliderProps {
   value?: number;
   max?: number;
 }
 export function SimpleSlider({ value = 0, max = 100 }: SimpleSliderProps) {
+  const width = useMemo(() => `${(value / (max || 1)) * 100}%`, [value, max]);
   return (
     <div className="slider">
-      <div
-        className="track track-0"
-        style={{ width: `${(value / max) * 100}%` }}
-      />
+      <div className="track track-0" style={{ width }} />
       <div className="track track-1" style={{ width: "100%" }} />
     </div>
   );
