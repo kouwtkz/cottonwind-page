e2504ee28ba019e664ca3de29f1f6c9bb9158550
kouwtkz <kouwtkz@cottonwind.com>
2025-06-12 20:09:09 +0900

    Viteの環境設定を見直したのとデバッグ用のグローバルドメインの指定を再びできるようにした

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+const envDev = loadEnv("development", process.cwd()) as ImportMetaEnv;
+
+const allowedHosts: string[] = [];
+if (envDev.VITE_LOCAL_TEST_DOMAIN) allowedHosts.push(envDev.VITE_LOCAL_TEST_DOMAIN);
+if (envDev.VITE_LOCAL_TEST_DOMAIN_2) allowedHosts.push(envDev.VITE_LOCAL_TEST_DOMAIN_2);
 
 export default defineConfig({
   server: {
     hmr: {
       overlay: false
-    }
+    },
+    allowedHosts,
   },
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
