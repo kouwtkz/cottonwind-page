2159542e48c7612222230f093c49b65a7158066c
わたかぜコウ <kouwtkz@gmail.com>
2026-01-14 18:19:12 +0900

    srcディレクトリの削除（GitLogだけはscriptsディレクトリに移動）

--- src/data/functions/MeeSqlD1.ts
+++
@@ -1,8 +0,0 @@
-import { MeeSqlClass } from "./MeeSqlClass";
-
-export class MeeSqlD1 extends MeeSqlClass<D1Database> {
-  override async select<K>(args: MeeSqlSelectProps<K>): Promise<K[]> {
-    const result = await super.select(args) as unknown as D1Result;
-    return result.results as K[];
-  }
-}
--- src/functions.ts
+++
@@ -1,8 +0,0 @@
-import { CommonHono } from "./types/HonoCustomType";
-
-export function honoTest(app: CommonHono) {
-  app.post("/test", async (c) => {
-    console.log(request.header("cf-connecting-ip") ?? "");
-    return c.json(request.header());
-  });
-}
