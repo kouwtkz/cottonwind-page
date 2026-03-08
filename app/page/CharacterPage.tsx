952d19587681ecc235240eca54cfcba3fbe44453
わたかぜコウ <kouwtkz@gmail.com>
2026-03-08 14:11:02 +0900

    キャラページにギャラリーのうちuploadsが入ってなかったので入れるようにした

--- app/page/CharacterPage.tsx
+++ app/page/CharacterPage.tsx
@@ -590,6 +590,7 @@ const defaultGalleryList = [
   { name: "3D" },
   { name: "picture" },
   { name: "parody", max: 12 },
+  { name: "uploads" },
   { name: "given", label: "Fanart", max: 40 },
 ] as GalleryItemType[];
 export function CharaDetail({ charaName }: { charaName: string }) {
