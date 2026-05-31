f3528e182e84a8db5086912052c85797f2b58d80
わたかぜコウ <kouwtkz@gmail.com>
2026-05-31 11:05:44 +0900

    untilやsince検索にdayやmonthなどを指定することでどのくらい前なのかに絞って検索できるようにした

--- app/data/find/findMee.ts
+++ app/data/find/findMee.ts
@@ -653,39 +653,47 @@ interface AutoAllotDateProps {
 }
 
 function AutoAllotDate({ value, replaceT = true, Normalize = true, dayFirst = false, dayLast = false, forceDayTime = false }: AutoAllotDateProps) {
-  if (replaceT) value = value.replace(/[\s_]/, "T"); else value = value.replace(/[_]/, "T");
-  const dateLength = value.split(/[-/]/, 3).length;
-  const nonTime = forceDayTime || !/[T\s]/.test(value);
-  if (forceDayTime && (dayFirst || dayLast)) value = value.replace(/[T\s][\d.:]+/, 'T00:00');
-  else if (nonTime) value = value.replace(/([\d.:])(\+[\d:]+|Z|)$/, "$1T00:00$2")
-
-  if (Normalize && /[T]/.test(value)) {
-    value = value.replace(/(\d+)[-/]?(\d*)[-/]?(\d*)T(\d*):?(\d*):?(\d*)/, (m, m1, m2, m3, m4, m5, m6) => {
-      let dateStr: string[] = []
-      if (m1) dateStr.push(`000${m1}`.slice(-4));
-      if (m2) dateStr.push(`0${m2}`.slice(-2));
-      if (m3) dateStr.push(`0${m3}`.slice(-2));
-      let timeStr: string[] = []
-      if (m4 + m5 === "0000") timeStr.push("00", "00");
-      else {
-        if (m4) timeStr.push(`0${m4}`.slice(-2));
-        if (m5) timeStr.push(`0${m5}`.slice(-2));
-      }
-      if (m6) timeStr.push(`0${m6}`.slice(-2));
-      return dateStr.join("-") + "T" + timeStr.join(":");
-    });
-  }
-
   let time: Date;
-  if (value.endsWith("Z") || /\+/.test(value))
-    time = new Date(value);
-  else
-    time = new Date(`${value}+09:00`);
-  if (dayLast && nonTime) {
-    if (dateLength === 1) time.setUTCFullYear(time.getUTCFullYear() + 1);
-    else if (dateLength === 2) time.setUTCMonth(time.getUTCMonth() + 1);
-    else time.setUTCDate(time.getUTCDate() + 1);
-    time.setUTCMilliseconds(-1);
+  const daysMatch = value.match(/(-?[\d]+)(years?|months?|days?)/i);
+  if (daysMatch) {
+    time = new Date();
+    if (/year/i.test(daysMatch[2])) time.setFullYear(time.getFullYear() - Number(daysMatch[1]));
+    else if (/month/i.test(daysMatch[2])) time.setMonth(time.getMonth() - Number(daysMatch[1]));
+    else if (/day/i.test(daysMatch[2])) time.setDate(time.getDate() - Number(daysMatch[1]));
+  } else {
+    if (replaceT) value = value.replace(/[\s_]/, "T"); else value = value.replace(/[_]/, "T");
+    const dateLength = value.split(/[-/]/, 3).length;
+    const nonTime = forceDayTime || !/[T\s]/.test(value);
+    if (forceDayTime && (dayFirst || dayLast)) value = value.replace(/[T\s][\d.:]+/, 'T00:00');
+    else if (nonTime) value = value.replace(/([\d.:])(\+[\d:]+|Z|)$/, "$1T00:00$2")
+
+    if (Normalize && /[T]/.test(value)) {
+      value = value.replace(/(\d+)[-/]?(\d*)[-/]?(\d*)T(\d*):?(\d*):?(\d*)/, (m, m1, m2, m3, m4, m5, m6) => {
+        let dateStr: string[] = []
+        if (m1) dateStr.push(`000${m1}`.slice(-4));
+        if (m2) dateStr.push(`0${m2}`.slice(-2));
+        if (m3) dateStr.push(`0${m3}`.slice(-2));
+        let timeStr: string[] = []
+        if (m4 + m5 === "0000") timeStr.push("00", "00");
+        else {
+          if (m4) timeStr.push(`0${m4}`.slice(-2));
+          if (m5) timeStr.push(`0${m5}`.slice(-2));
+        }
+        if (m6) timeStr.push(`0${m6}`.slice(-2));
+        return dateStr.join("-") + "T" + timeStr.join(":");
+      });
+    }
+
+    if (value.endsWith("Z") || /\+/.test(value))
+      time = new Date(value);
+    else
+      time = new Date(`${value}+09:00`);
+    if (dayLast && nonTime) {
+      if (dateLength === 1) time.setUTCFullYear(time.getUTCFullYear() + 1);
+      else if (dateLength === 2) time.setUTCMonth(time.getUTCMonth() + 1);
+      else time.setUTCDate(time.getUTCDate() + 1);
+      time.setUTCMilliseconds(-1);
+    }
   }
   return time;
 }
