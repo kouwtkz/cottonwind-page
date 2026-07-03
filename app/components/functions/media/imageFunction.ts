ad910f2d47d72648b5314c5a7484e0356a36796f
わたかぜコウ <kouwtkz@gmail.com>
2026-07-04 00:35:42 +0900

    imageFunctionの使ってない関数を間違えて編集しないように削除した

--- app/components/functions/media/imageFunction.ts
+++ app/components/functions/media/imageFunction.ts
@@ -1,5 +1,3 @@
-import { getCountList } from "../arrayFunction";
-
 export function getImageAlbumMap(imageAlbumEnv?: ImageAlbumEnvType[], albums?: string[]) {
   const imageAlbumMap = new Map<string, ImageAlbumType>();
   imageAlbumEnv?.forEach((album) => {
@@ -15,55 +13,6 @@ export function getImageAlbumMap(imageAlbumEnv?: ImageAlbumEnvType[], albums?: s
     if (!imageAlbumMap.has(album)) imageAlbumMap.set(album, { name: album, list: [] });
   })
   return imageAlbumMap;
-}
-
-export function getImageObjectMap(imagesData: ImageDataType[], imageAlbumEnv?: ImageAlbumEnvType[]) {
-  const imageAlbumMap = getImageAlbumMap(imageAlbumEnv);
-  const imagesMap = new Map<string, ImageType>();
-  imagesData
-    .filter((v) => v.version && v.src)
-    .forEach((v) => {
-      console.log(v);
-      if (v.album && !imageAlbumMap.has(v.album)) imageAlbumMap.set(v.album, { name: v.album, list: [] });
-      const albumObject = v.album ? imageAlbumMap.get(v.album) : undefined;
-      const item: ImageType = {
-        ...toImageType(v, imageAlbumMap),
-        albumObject,
-      };
-      if (!imagesMap.has(item.key)) {
-        albumObject?.list.push(item);
-        imagesMap.set(item.key, item);
-      }
-    });
-  return { imagesMap, imageAlbumMap }
-}
-
-export function toImageType(data: ImageDataType, albumsMap?: Map<string, ImageAlbumType>): ImageType {
-  const albumObject = data.album && albumsMap ? albumsMap.get(data.album) : undefined;
-  const lastmod = data.lastmod ? new Date(data.lastmod) : undefined;
-  return {
-    ...data,
-    type: data.type ? data.type as imageKindType : AutoImageItemType(data.embed, albumObject?.type),
-    albumObject,
-    tags: data.tags?.split(","),
-    characters: data.characters?.split(","),
-    copyright: data.copyright?.split(","),
-    pickup:
-      typeof data.pickup === "number" ? Boolean(data.pickup) : undefined,
-    draft:
-      typeof data.draft === "number" ? Boolean(data.draft) : undefined,
-    wh: `${data.width}x${data.height}`,
-    creationTime: new TimeClass(data.creationTime || ""),
-    time: data.time ? new Date(data.time) : undefined,
-    mtime: data.mtime ? new Date(data.mtime) : undefined,
-    lastmod,
-    update: undefined,
-    new: undefined,
-    next: undefined,
-    previous: undefined,
-    schedule: lastmod && lastmod.getTime() > Date.now(),
-    data
-  };
 }
 
 export function AutoImageItemType(embed?: OrNull<string>, albumType?: OrNull<string>): imageKindType {
