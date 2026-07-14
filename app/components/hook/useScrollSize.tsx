fb813b5a3f67b07e720e71e26996374443bbd3fc
わたかぜコウ <kouwtkz@gmail.com>
2026-07-14 11:17:49 +0900

    useWindowScrollSizeにページ移動も反映するようにした

--- app/components/hook/useScrollSize.tsx
+++ app/components/hook/useScrollSize.tsx
@@ -2,18 +2,25 @@ import { useEffect, useState } from "react";
 import { CreateState } from "../state/CreateState";
 import { useIsLoading } from "../state/SetState";
 import { useWindowSize } from "./useWindowSize";
+import { useLocation } from "react-router";
 
 export const useWindowScrollSize = CreateState<[number, number]>([0, 0]);
 
 export function WindowScrollSizeState() {
   const ww = useWindowSize()[0][0];
+  const location = useLocation();
   const isLoading = useIsLoading()[0];
   const setSize = useWindowScrollSize()[1];
   useEffect(() => {
     if (!isLoading) {
-      setSize([document.body.scrollWidth, document.body.scrollHeight]);
+      setSize((state) => {
+        const w = document.body.scrollWidth;
+        const h = document.body.scrollHeight;
+        if (w !== state[0] || h !== state[1]) return [w, h];
+        else return state;
+      });
     }
-  }, [ww, isLoading]);
+  }, [ww, isLoading, location]);
   return <></>;
 }
 
